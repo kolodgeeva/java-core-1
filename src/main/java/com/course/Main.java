2bce25b8f31306d6122b05b541d9815d559df684
Mariana Kolodzheieva <os-mari.kolodzheieva@rakuten.com>
2022-07-08 19:30:41 +0100

    Add reader

--- src/main/java/com/course/Main.java
+++ src/main/java/com/course/Main.java
@@ -1,4 +1,8 @@
 package com.course;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
 }
